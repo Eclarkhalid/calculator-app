ce449d3db044a21dcea94623dd25a9a7d01822d6
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 19:14:18 +0300

    Fix the division and multiplication

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -12,46 +12,30 @@ const Calculator = () => {
   const [currentOperation, setCurrentOperation] = useState(null);
 
 
+
+
   const handleClick = (value: string) => {
-   switch (value) {
-    case "+":
-    case "-":
+    switch (value) {
+      case "+":
+      case "-":
       case "*":
-        case "/":
-          if (firstOperand === null) {
-            setFirstOperand(display);
-          } else {
-            performCalculation(display);
-          }
-          setCurrentOperation(value);
-          setDisplay(display + value);
-          break;
-    case "=":
-      if (firstOperand !== null && currentOperation) {
-        switch (currentOperation) {
-          case "+":
-            setDisplay(add(firstOperand, display));
-            break;
-          case "-":
-            setDisplay(substract(firstOperand, display));
-            break;
-          case "*":
-            setDisplay(multiply(firstOperand, display));
-            break;
-          case "/":
-            try {
-              setDisplay(divide(firstOperand, display));
-            } catch (e) {
-              setDisplay(e.message);
-            }
-            break;
-            default:
-              break;
+      case "/":
+        if (firstOperand === null) {
+          setFirstOperand(parseFloat(display));
+        } else if (currentOperation) {
+          setFirstOperand(performCalculation());
         }
-        setFirstOperand(null);
-        setCurrentOperation(null);
-      }
-      break;
+        setCurrentOperation(value);
+        setDisplay((prev) => prev + value);
+        break;
+      case "=":
+        if (firstOperand !== null && currentOperation) {
+          const result = performCalculation();
+          setFirstOperand(null);
+          setCurrentOperation(null);
+          setDisplay(result.toString());
+        }
+        break;
 
       case "C":
         setDisplay("");
@@ -59,36 +43,38 @@ const Calculator = () => {
         setCurrentOperation(null);
         break;
 
-        default:
-          setDisplay(display + value);
-          break;
-   }
+      default:
+        setDisplay((prev) => prev + value);
+        break;
+    }
   };
 
   const performCalculation = () => {
+    const secondOperandStartIndex = firstOperand.toString().length + 1;
+    const secondOperand = parseFloat(display.slice(secondOperandStartIndex));
+
     switch (currentOperation) {
       case "+":
-        setDisplay((prev) => prev.slice(0, -1) + add(firstOperand, display.slice(0, -1)));
-        case "-":
-        setDisplay((prev) => prev.slice(0, -1) + substract(firstOperand, display.slice(0, -1)));
-        case "*":
-        setDisplay((prev) => prev.slice(0, -1) + multiply(firstOperand, display.slice(0, -1)));
-        case "/":
+        return add(parseFloat(firstOperand), secondOperand);
+      case "-":
+        return substract(parseFloat(firstOperand), secondOperand);
+      case "*":
+        return multiply(parseFloat(firstOperand), secondOperand);
+      case "/":
         try {
-          setDisplay((prev) => prev.slice(0, -1) + divide(firstOperand, display.slice(0, -1)));
+          return divide(parseFloat(firstOperand), secondOperand);
         } catch (e) {
           setDisplay(e.message);
-          
+
         }
         break;
-          default:
-            break;
+      default:
+        break;
     }
-    setFirstOperand(null);
   }
   const buttons = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+", "C"];
   return <>
-    <div className="p-10 grid grid-cols-4 gap-4">
+    <div className="p-10 grid grid-cols-4 gap-4 max-w-lg">
       <Display value={display} />
       {
         buttons.map((button, index) => (
--- components/display.tsx
+++ components/display.tsx
@@ -1,6 +1,6 @@
 const Display = ({value}) => {
   return <>
-  <div className="col-span-4 w-full bg-gray-800 text-white text-right py-6 px-10 rounded">
+  <div className="flex justify-end col-span-4 h-20 w-full bg-gray-800 text-white text-right py-6 px-10 rounded">
     {value}
   </div>
   </>;
