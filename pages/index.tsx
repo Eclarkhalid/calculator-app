0dcf67dfba269e35520ee4bc5d356ffb6148aa13
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 19:21:50 +0300

    Update the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import Head from "next/head";
+import { BsCalculator } from 'react-icons/bs'
 import Calculator from "@/components/calculator";
 
 export default function Home() {
   return <>
   <div className="bg-gray-900 min-h-screen">
-    <h1 className="text-white text-center">
+    <h1 className="flex justify-center items-center font-medium gap-5 p-20 text-white text-center">
       EK Simple Calculator <BsCalculator />
     </h1>
   <div className="flex items-center justify-center ">
