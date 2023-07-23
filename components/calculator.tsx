39065bf75352eb77bcda05859ca555e3207c746a
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 17:33:30 +0300

    Creating the display component

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Display from "./display";
 
 const Calculator = () => {
   const [display, setDisplay] = useState("");
 
   const handleClick = (value: string) => {
-    if(value === "=") {
-      try{
+    if (value === "=") {
+      try {
         setDisplay(eval(display));
       } catch {
         setDisplay("Error");
@@ -18,15 +19,15 @@ const Calculator = () => {
   };
   const buttons = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+", "C"];
   return <>
-  <div className="p-10 grid grid-cols-4 gap-4">
-    <Display value={display} />
-    {
-      buttons.map((button, index) => (
-        <Button key={index} value={button} onclick={() => handleClick(button)} />
-      ))
-    }
-  </div>
+    <div className="p-10 grid grid-cols-4 gap-4">
+      <Display value={display} />
+      {
+        buttons.map((button, index) => (
+          <Button key={index} value={button} onclick={() => handleClick(button)} />
+        ))
+      }
+    </div>
   </>
 }
- 
+
 export default Calculator;
---
+++ components/display.tsx
@@ -0,0 +1,9 @@
+const Display = ({value}) => {
+  return <>
+  <div className="col-span-4 bg-gray-800 text-white text-right py-6 px-10 rounded">
+    {value}
+  </div>
+  </>;
+}
+ 
+export default Display;
