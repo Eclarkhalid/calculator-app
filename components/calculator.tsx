4f77993b003a4c1ed741caf8db0b2328696e61ea
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 18:48:52 +0300

    Update the calculator component

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -18,9 +18,13 @@ const Calculator = () => {
     case "-":
       case "*":
         case "/":
-          setFirstOperand(display);
+          if (firstOperand === null) {
+            setFirstOperand(display);
+          } else {
+            performCalculation(display);
+          }
           setCurrentOperation(value);
-          setDisplay("");
+          setDisplay(display + value);
           break;
     case "=":
       if (firstOperand !== null && currentOperation) {
@@ -60,6 +64,28 @@ const Calculator = () => {
           break;
    }
   };
+
+  const performCalculation = () => {
+    switch (currentOperation) {
+      case "+":
+        setDisplay((prev) => prev.slice(0, -1) + add(firstOperand, display.slice(0, -1)));
+        case "-":
+        setDisplay((prev) => prev.slice(0, -1) + substract(firstOperand, display.slice(0, -1)));
+        case "*":
+        setDisplay((prev) => prev.slice(0, -1) + multiply(firstOperand, display.slice(0, -1)));
+        case "/":
+        try {
+          setDisplay((prev) => prev.slice(0, -1) + divide(firstOperand, display.slice(0, -1)));
+        } catch (e) {
+          setDisplay(e.message);
+          
+        }
+        break;
+          default:
+            break;
+    }
+    setFirstOperand(null);
+  }
   const buttons = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+", "C"];
   return <>
     <div className="p-10 grid grid-cols-4 gap-4">
