5772cfb48b333a6ef3d8db4f98f2bad2ef2f82c3
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 19:17:56 +0300

    Add a heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,13 @@ import Calculator from "@/components/calculator";
 
 export default function Home() {
   return <>
-  <div className="flex items-center justify-center min-h-screen bg-gray-900">
+  <div className="bg-gray-900 min-h-screen">
+    <h1 className="text-white text-center">
+      EK Simple Calculator <BsCalculator />
+    </h1>
+  <div className="flex items-center justify-center ">
     <Calculator />
+  </div>
   </div>
   </>;
 }
