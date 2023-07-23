a9b29a058172e4dee639555dd2284e442353f9a1
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 20:17:02 +0300

    Fix the deployment error

--- components/display.tsx
+++ components/display.tsx
@@ -1,9 +1,16 @@
-const Display = ({value}) => {
-  return <>
-  <div className="flex justify-end col-span-4 h-20 w-full bg-gray-800 text-white text-right py-6 px-10 rounded">
-    {value}
-  </div>
-  </>;
+import React from 'react';
+
+// Define the type for the value prop
+interface DisplayProps {
+  value: string; // Replace 'string' with the appropriate type of the 'value' prop
 }
- 
+
+const Display = ({ value }: DisplayProps) => {
+  return (
+    <div className="flex justify-end col-span-4 h-20 w-full bg-gray-800 text-white text-right py-6 px-10 rounded">
+      {value}
+    </div>
+  );
+};
+
 export default Display;
