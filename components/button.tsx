7ff0746db365ed166ca535ed73c7646bb3cdc915
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-23 19:33:17 +0300

    Update the button to fix deplyoment error

--- components/button.tsx
+++ components/button.tsx
@@ -1,11 +1,13 @@
 import { Button } from "./ui/button";
 
-const Buttons = ({value, onClick}) => {
-  return <>
-  <Button onClick={onClick} className="bg-gray-800 text-white w-20 py-3 rounded">
-    {value}
-  </Button>
-  </>;
-}
+const Buttons = ({ value, onClick }: { value: string, onClick: () => void }) => {
+  return (
+    <>
+      <Button onClick={onClick} className="bg-gray-800 text-white w-20 py-3 rounded">
+        {value}
+      </Button>
+    </>
+  );
+};
  
 export default Buttons;
